58190a4e0b571f943e2b22af05442fc6cbe3f71a
scroogemcfawk <scroogemcfawk@gmail.com>
2023-09-26 14:38:06 +0700

    Player rework

--- audioTest.js
+++ audioTest.js
@@ -1,74 +1,165 @@
-// var ad = navigator.mediaDevices.getDisplayMedia({audio: true})
-var audio = document.getElementById("audi")
-var jsonArray
+const main = document.getElementById("main")
+const exampleJsonPath = "api-example.json"
+const testMode = true
+const debugMode = true
+const jsonLink = "https://marigostra.ru/persist/proj-player/fragments.json"
 
-function getJSON(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true)
-    xhr.responseType = 'json'
-    xhr.onload = function() {
-        var status = xhr.status
-        if (status === 200) {
-            callback(null, xhr.response)
-        } else {
-            callback(status, xhr.response)
-        }
+const PLAY_LABEL = "Play"
+const STOP_LABEL = "Stop"
+
+const audio = document.createElement("audio")
+const fragmentLabel = document.createElement("label")
+const fragmentText = document.createElement("div")
+const br = document.createElement("br")
+const prevButton = document.createElement("button")
+const playStopButton = document.createElement("button")
+const nextButton = document.createElement("button")
+
+let jsonFragments
+
+function log(msg) {
+    if (debugMode) {
+        console.log(msg)
     }
-    xhr.send()
 }
 
-function responseHandler(status, data) {
-    if (status !== null) {
-        console.log("Something went wrong")
+function initElements() {
+    function initButton(button, id, innerText, disabled) {
+        button.id = id
+        button.innerText = innerText
+        button.disabled = disabled
+    }
+
+    initButton(prevButton, "prevButton", "Prev", true)
+    initButton(playStopButton, "playStopButton", PLAY_LABEL, false)
+    initButton(nextButton, "nextButton", "Next", false)
+    audio.id = "audio"
+    fragmentLabel.id = "fragmentLabel"
+    fragmentText.id = "fragmentText"
+}
+
+function setUpButtons() {
+    const b = document.getElementById("loadFragmentsButton")
+    b.remove()
+    main.appendChild(fragmentLabel)
+    main.appendChild(br)
+    main.appendChild(prevButton)
+    main.appendChild(playStopButton)
+    main.appendChild(nextButton)
+    main.appendChild(fragmentText)
+
+    resolveLoadingFragments()
+}
+
+function resolveLoadingFragments() {
+    if (testMode) {
+        loadFragments(exampleJsonPath)
     } else {
-        console.log(data)
+        loadFragments(jsonLink)
     }
 }
 
-function loadJSONArray() {
-    getJSON("https://marigostra.ru/persist/proj-player/fragments.json", responseHandler)
+function loadFragments(source) {
+    fetch(source)
+        .then(res => res.json())
+        .then(out => {
+            jsonFragments = out
+            setFragment(0)
+        })
+        .catch(err => {
+            console.log("Error " + err)
+        })
 }
 
+function setFragment(n) {
+    if (jsonFragments.length <= 0) {
+        console.log("Fragment set is empty")
+        return
+    }
+    if (jsonFragments.length <= n) {
+        console.log("Illegal fragment number parameter")
+        return
+    }
+    fragmentLabel.innerText = (n + 1) + " / " + jsonFragments.length
 
-function playCustom() {
-    var s = parseFloat(document.getElementById("startV").value)
-    var e = parseFloat(document.getElementById("endV").value)
-    playAB(s, e)
+    const fragment = jsonFragments[n]
+    if (audio.src !== fragment.url) {
+        audio.src = fragment.url
+    }
+    fragmentText.innerText = fragment.text
+    playStopButton.innerText = PLAY_LABEL
+    playStopButton.onclick = () => {
+        playHandler(fragment.timeBegin, fragment.timeEnd)
+    }
+    if (n > 0) {
+        prevButton.disabled = false
+        prevButton.onclick = () => {
+            if (!audio.paused) {
+                audio.pause()
+            }
+            setFragment(n - 1)
+        }
+    } else {
+        prevButton.disabled = true
+    }
+    if (n < jsonFragments.length - 1) {
+        nextButton.disabled = false
+        nextButton.onclick = () => {
+            if (!audio.paused) {
+                audio.pause()
+            }
+            setFragment(n + 1)
+        }
+    } else {
+        nextButton.disabled = true
+    }
 }
 
-function playAB(a, b) {
+function playHandler(a, b) {
+    playStopButton.innerText = STOP_LABEL
+    playStopButton.onclick = () => {
+        stopHandler(a, b)
+    }
+    playAB(a, b)
+}
+
+function stopHandler(a, b) {
+    if (!audio.paused) {
+        audio.pause()
+    }
     audio.currentTime = a
-    console.log("Starting at " + a)
+    playStopButton.innerText = PLAY_LABEL
+    playStopButton.onclick = () => {
+        playHandler(a, b)
+    }
+}
+function playAB(a, b) {
+    if (a > b) {
+        log("Illegal function arguments")
+        return
+    }
+    audio.currentTime = a
     audio.play()
     var duration = (b - a) * 1000
-    console.log(duration)
-    killWhenOverB(b)
+    log("Playing fragment: " + a.toFixed(2) + " - " + b.toFixed(2) +
+        ".\nFragment duration: " + (duration / 1000).toFixed(2) + "s.")
+    killWhenOverB(b, a)
 }
 
-function killWhenOverB(b) {
-    setTimeout(
-        function () {
+function killWhenOverB(b, a) {
+    // 200ms is enough for human reaction to not notice the difference imo
+    setTimeout(function () {
+        if (!audio.paused) {
             if (audio.currentTime > b) {
                 audio.pause()
+                audio.currentTime = a
+                playStopButton.innerText = PLAY_LABEL
+                playStopButton.onclick = () => {
+                    playHandler(a, b)
+                }
             } else {
-                killWhenOverB(b)
+                killWhenOverB(b, a)
             }
-        }, 200)
-}
-
-function stopAudio() {
-    audio.pause()
-}
-
-function resumeAudio() {
-    audio.play()
-}
-
-function whatTime() {
-    console.log(audio.currentTime)
-}
-
-function loadMusic() {
-    var link = document.getElementById("music").value
-    audio.src = link
+        }
+    }, 200)
 }
